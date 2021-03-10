09a6f9e5686f3679fe994de76a62b26bea486a4a
Idan Miara <idan@miara.com>
2021-03-10 11:53:33 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+# This setup.py file is licensed under MIT license.
+# Other files on this project are licensed under a different license (see LICENSE.txt for details)
+
+from setuptools import setup
+
+package_name = 'gdal-sid'
+v = (3, 2, 6)
+url = 'https://github.com/talos-gis/gdal-sid'
+maintainer = 'Idan Miara'
+maintainer_email = 'idan@miara.com'
+
+version = '.'.join(str(v) for v in v)
+# a gdal plugin version A.B.C is compatible with gdal version A.B.D for any D.
+install_requires = [f'gdal>={v[0]}.{v[1]},<{v[0]}.{v[1]+1}']
+readme = open('../README.md', encoding="utf-8").read()
+readme_type = 'text/markdown'
+
+setup(
+    name=package_name,
+    version=version,
+    url=url,
+    maintainer=maintainer,
+    maintainer_email=maintainer_email,
+    packages=['osgeo', 'osgeo.gdalplugins'],
+    install_requires=install_requires,
+    long_description=readme,
+    long_description_content_type=readme_type,
+    package_data={"": ["*.dll"]},
+)
